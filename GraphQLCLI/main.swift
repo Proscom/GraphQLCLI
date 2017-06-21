4135dba81beb601baf221003bb3b6c48f7850202
MAXIM KOLESNIK <makleso6@gmail.com>
2017-06-21 17:04:45 +0300

    Initial Commit

---
+++ GraphQLCLI/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GraphQLCLI
+//
+//  Created by MAXIM KOLESNIK on 21.06.17.
+//  Copyright © 2017 MAXIM KOLESNIK. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
