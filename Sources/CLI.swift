4d1907284a4d3e4fc1c7e97f6b31e45bcca9c84c
MAXIM KOLESNIK <makleso6@gmail.com>
2017-06-23 12:51:51 +0300

    fix error

--- Sources/CLI.swift
+++ Sources/CLI.swift
@@ -108,6 +108,9 @@ struct Program {
             return dictionary
         } catch {
             print(error.localizedDescription)
+            print("code: \((error as NSError).code as Any)")
+            print("domain: \((error as NSError).domain as Any)")
+            print("userInfo: \((error as NSError).userInfo as Any)")
             throw CodegenError.codegenResultNotCreate
         }
 
